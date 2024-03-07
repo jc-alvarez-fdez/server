4b85385b576bc18185accb4402fb32c7a9216103
jc-alvarez-fdez <jc.alvarez.fdez@gmail.com>
2024-03-07 11:50:08 +0100

    configuración de las rutas inicio

--- dist/models/server.js
+++ dist/models/server.js
@@ -4,15 +4,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const producto_route_1 = __importDefault(require("../routes/producto.route"));
 class Server {
     constructor() {
         this.app = (0, express_1.default)();
-        this.port = '3001';
+        this.port = process.env.PORT || "3001";
         this.listen();
+        this.routes();
     }
     listen() {
         this.app.listen(this.port, () => {
             console.log(`Aplicación corriendo en el puerto ${this.port}`);
+        });
+    }
+    routes() {
+        this.app.get('/', (req, res) => {
+            res.json({
+                msg: 'API working'
+            });
+            this.app.use('/api/productos', producto_route_1.default);
         });
     }
 }
--- src/models/server.ts
+++ src/models/server.ts
@@ -1,4 +1,5 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
+import  routesProducto from '../routes/producto.route'
 
 class Server {
 
@@ -7,8 +8,9 @@ class Server {
 
     constructor() {
         this.app = express();
-        this.port = '3001';
+        this.port = process.env.PORT || "3001";
         this.listen();
+        this.routes();
 
     }
 
@@ -16,11 +18,18 @@ class Server {
         this.app.listen(this.port, () => {
             console.log(`Aplicación corriendo en el puerto ${this.port}`)
         })
+    }    
+        
+        routes() {
+            this.app.get('/', (req: Request, res: Response) => {
+                res.json ({
+                    msg: 'API working'
+                })
+                this.app.use('/api/productos', routesProducto);
 
+            })
+        }
     }
 
-    
-
-}
-
+ 
 export default Server;
